201a3862fb7a031042d8ceef399f56bac6341e6c
MaheswaraKaindra <19623197@std.stei.itb.ac.id>
2025-01-09 21:55:43 +0700

    fix: prolog_introduction_1.pl

--- Computational Logic/prolog_introduction_1.pl
+++ Computational Logic/prolog_introduction_1.pl
@@ -1,8 +1,10 @@
+/* Fakta */
 ibu(emy, charles).
 ibu(emy, david).
 ibu(emy, randy).
 ibu(maria, farah).
 
+/* Aturan */
 saudara_kandung(Anak1, Anak2) :-
     ibu(Ibu, Anak1),
     ibu(Ibu, Anak2),
